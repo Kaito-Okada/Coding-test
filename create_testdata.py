771b431af209147d049d11e2ee51a6fdd905424d
Kaito Okada <pdrj11bt@s.okayama-u.ac.jp>
2021-02-05 12:30:55 +0900

    fix:上限テストデータ作成用スクリプトの修正

--- create_testdata.py
+++ create_testdata.py
@@ -1,28 +1,8 @@
 import datetime
-import random
 
 year =2020
 month =12
 day =31
-server_address_list =[
-    '10.20.30.1/10',
-    '10.20.30.2/10',
-    '10.20.30.3/10',
-    '11.22.33.1/10',
-    '11.22.33.2/10',
-]
-response_list = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '-',
-]
 
 with open('testdata_for_max.txt', mode='w') as f:
     for h in range(0,24):
@@ -30,10 +10,6 @@ with open('testdata_for_max.txt', mode='w') as f:
             for s in range(0,60):
                 dt = datetime.datetime(year, month, day, h, m, s)
                 dt_str = dt.strftime('%Y%m%d%H%M%S')
-
-                server_address = random.choice(server_address_list)
-                res  = random.choice(response_list)
-
-                l = dt_str + ',' + server_address + ',' + res +'\n'
+                l = dt_str + ',10.20.30.1/16,2\n'
                 
                 f.write(l)
